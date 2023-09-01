44234caf57a47acfdc7d493b08b54b17dba7a3f0
VF-ROOT\jgonz395 <jaime.gonzalez1@vodafone.com>
2023-09-01 12:29:03 +0200

    Implements the option to retrieve file names in ascending order

--- src/LockedMeApp/LockedMeApp.java
+++ src/LockedMeApp/LockedMeApp.java
@@ -6,6 +6,23 @@ import java.util.Arrays;
 public class LockedMeApp {
 
     public static void main(String[] args) {
+
+        displayWelcomeScreen();
+
+        int userChoice = 1;
+
+        switch (userChoice) {
+            case 1:
+                retrieveFileNamesInAscendingOrder();
+                break;
+            // Other cases will be added here
+            default:
+                System.out.println("Invalid choice. Please select a valid option (1-6).");
+        }
+    }
+
+    // Method to display the welcome screen and menu options
+    private static void displayWelcomeScreen() {
         System.out.println("**********************************************");
         System.out.println("*          Welcome to LockedMe.com           *");
         System.out.println("*                Developed by                *");
@@ -24,5 +41,33 @@ public class LockedMeApp {
 
     }
 
+
+
+    // Method to retrieve and display file names in ascending order
+    private static void retrieveFileNamesInAscendingOrder() {
+        System.out.println();
+        // Specify the directory path (change this to your desired directory)
+        String directoryPath = "src/files_dummy";
+
+        // Create a File object for the directory
+        File directory = new File(directoryPath);
+
+        // Check if the directory exists and is a directory
+        if (directory.exists() && directory.isDirectory()) {
+            // List the files in the directory
+            String[] fileNames = directory.list();
+
+            // Sort the file names in ascending order
+            Arrays.sort(fileNames);
+
+            // Display the sorted file name
+            System.out.println("File Names in Ascending Order:");
+            for (String fileName : fileNames) {
+                System.out.println(fileName);
+            }
+        } else {
+            System.out.println("Invalid directory path.");
+        }
+    }
 }
 
