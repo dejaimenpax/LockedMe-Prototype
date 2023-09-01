f50a6ff23ee8455d4583fa10c48bb4f12195893f
VF-ROOT\jgonz395 <jaime.gonzalez1@vodafone.com>
2023-09-01 12:28:18 +0200

    Implements the code to display the welcome screen

---
+++ src/LockedMeApp/LockedMeApp.java
@@ -0,0 +1,28 @@
+package LockedMeApp;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class LockedMeApp {
+
+    public static void main(String[] args) {
+        System.out.println("**********************************************");
+        System.out.println("*          Welcome to LockedMe.com           *");
+        System.out.println("*                Developed by                *");
+        System.out.println("*           Jaime González Casero            *");
+        System.out.println("**********************************************");
+        System.out.println();
+        System.out.println("User Options:");
+        System.out.println("1. Retrieve file names in ascending order");
+        System.out.println("2. Add a user-specified file");
+        System.out.println("3. Delete a user-specified file");
+        System.out.println("4. Search for a user-specified file");
+        System.out.println("5. Navigate back to the main context");
+        System.out.println("6. Close the application");
+        System.out.println();
+        System.out.print("Please enter your choice (1-6): ");
+
+    }
+
+}
+
